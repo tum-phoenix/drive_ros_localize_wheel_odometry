a69fac461fdaa2b57906fe159bfeddeb77aaeaa2
Unknown <dev-faha@t-online.de>
2018-02-24 11:14:51 +0100

    also write pos to output odom

--- src/filter_node.cpp
+++ src/filter_node.cpp
@@ -7,6 +7,9 @@
 ros::Publisher odom;
 int encoder_ct = 0;
 
+bool use_rel_pos = true;
+double rel_pos = 0;
+
 nav_msgs::Odometry odom_out;
 
 
@@ -19,7 +22,7 @@ void encoderCallback(const drive_ros_msgs::VehicleEncoder::ConstPtr& msg)
   odom_out.child_frame_id = ""; // this is not a real odom information
 
   // caculate velocity
-  double vel = 0, vel_var = 0;
+  double vel = 0, vel_var = 0, pos = 0, pos_var = 0;
   if(0 == encoder_ct)
   {
     ROS_ERROR("We need at least one encoder, to work properly!");
@@ -31,13 +34,35 @@ void encoderCallback(const drive_ros_msgs::VehicleEncoder::ConstPtr& msg)
     for(int i=0; i<encoder_ct; i++){
       vel     += msg->encoder[i].vel;
       vel_var += msg->encoder[i].vel_var;
+
+      if(use_rel_pos)
+      {
+        pos     += msg->encoder[i].pos_rel;
+        pos_var += msg->encoder[i].pos_rel_var;
+      }else{
+        pos     += msg->encoder[i].pos_abs;
+        pos_var += msg->encoder[i].pos_abs_var;
+      }
+
     }
+
     vel     = vel/(float)encoder_ct;
     vel_var = vel_var/(float)encoder_ct;
+    pos     = pos/(float)encoder_ct;
+    pos_var = pos_var/(float)encoder_ct;
+
+
 
   }
 
-  // save velocities in odom
+  // increment relative position
+  if(use_rel_pos){
+    pos = rel_pos += pos;
+  }
+
+  // save position and velocity in odom_out
+  odom_out.pose.pose.position.x = pos;
+  odom_out.pose.covariance[CovElem::lin_ang::linX_linX] = pos_var;
   odom_out.twist.twist.linear.x = vel;
   odom_out.twist.covariance[CovElem::lin_ang::linX_linX] = vel_var;
 
@@ -55,6 +80,8 @@ int main(int argc, char **argv)
   // get parameters
   encoder_ct = pnh.param<int>("encoder_ct", 1);
   ROS_INFO_STREAM("Loaded encoder_ct: " << encoder_ct);
+  use_rel_pos = pnh.param<bool>("use_rel_pos", true);
+  ROS_INFO_STREAM("Loaded use_rel_pos: " << use_rel_pos);
 
 
   // set all not used odom covariances to -1
